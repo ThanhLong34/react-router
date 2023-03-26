650f317b786c39cf5323f0067fb6547c12d87686
DragonDev <thanhlongedu0304@gmail.com>
2023-03-26 20:36:38 +0700

    Move renderRoutes function out

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,14 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { routes } from "./router";
 import Navigator from "./components/Navigator";
 
-function App() {
-	const renderRoutes = () =>
-		routes &&
-		routes.map((route, idx) => {
-			const Component = route.component;
-			return (
-				<Route key={idx} path={route.path} element={<Component />} />
-			)
-		});
+const renderRoutes = () =>
+	routes &&
+	routes.map((route, idx) => {
+		const Component = route.component;
+		return <Route key={idx} path={route.path} element={<Component />} />;
+	});
 
+function App() {
 	return (
 		<div className="App">
 			<Router>
