d58d923e5fe9f62bcd91ced0df01f50b86ffeab8
DragonDev <thanhlongedu0304@gmail.com>
2023-03-26 19:16:12 +0700

    Add base styles

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
+// styles
+import '@/assets/styles/reset.scss';
+import '@/assets/styles/main.scss';
+
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
 		<App />
